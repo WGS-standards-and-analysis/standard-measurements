b552de960a6e67d8cc0f92191ec9625c207b043f
Lee Katz - Aspen <gzu2@cdc.gov>
2018-09-06 10:34:41 -0400

    unit test for fastq average quality

---
+++ tests/fastqAvgQuality.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Tests all fastq average quality scripts
+
+set -e
+basedir=$(dirname $0)
+thisscript=$(basename $0)
+tmpdir=$(mktemp --directory $thisscript.XXXXXX)
+trap ' { rm -rf $tmpdir; } ' EXIT
+
+# Rounding to the millionths place
+R1qual=37.787117
+R2qual=35.684931
+R1="$basedir/data/CFSAN023471_truncated_1.fastq.gz"
+R2="$basedir/data/CFSAN023471_truncated_2.fastq.gz"
+
+# Run N50 and round to the millionths place
+perlR1Qual=$(perl $basedir/../scripts/perl/fastqAvgQuality.pl $R1)
+perlR1Qual=$(printf "%0.6f" $perlR1Qual)
+perlR2Qual=$(perl $basedir/../scripts/perl/fastqAvgQuality.pl $R2)
+perlR2Qual=$(printf "%0.6f" $perlR2Qual)
+
+# Test N50 values
+if (( $(echo "$perlR1Qual != $R1qual" | bc -l) )); then
+  echo "ERROR: fastqAvgQual.pl did not produce the expected result on R1."
+  exit 1
+fi
+if (( $(echo "$perlR2Qual != $R2qual" | bc -l) )); then
+  echo "ERROR: fastqAvgQual.pl did not produce the expected result on R2."
+  exit 1
+fi
+
+echo "Fastq average quality tests passed!"
